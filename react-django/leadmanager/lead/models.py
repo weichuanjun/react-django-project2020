64bf1f216892b690f6caa6fd913318a9f5d7dad1
WEICHUANJUN <weichuanjun0528@gmail.com>
2020-05-26 14:37:05 +0900

    change

--- react-django/leadmanager/lead/models.py
+++ react-django/leadmanager/lead/models.py
@@ -4,7 +4,7 @@ from django.db import models
 
 
 class Lead(models.Model):
-    name = models.models.CharField(max_length=50)
-    email = models.models.EmailField(max_length=254, unique=True)
-    message = models.models.CharField(_(""), max_length=50, blank=True)
-    creat_at = models.models.DateTimeField(_(""),  auto_now_add=True)
+    name = models.CharField(max_length=50)
+    email = models.EmailField(max_length=254, unique=True)
+    message = models.CharField(max_length=50, blank=True)
+    creat_at = models.DateTimeField(auto_now_add=True)
--- react-django/leadmanager/leadmanager/settings.py
+++ react-django/leadmanager/leadmanager/settings.py
@@ -37,7 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'leads',
+    'lead',
     'rest_framework'
 ]
 
