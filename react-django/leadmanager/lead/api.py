29a180cdf0978c5c961662fe1579c036ebf463a2
WEICHUANJUN <weichuanjun0528@gmail.com>
2020-05-27 09:50:55 +0900

    5/27

---
+++ react-django/leadmanager/lead/api.py
@@ -0,0 +1,13 @@
+from lead.models import Lead
+from rest_framework import viewsets, permissions
+from .serializers import LeadSerializer
+
+# viewset
+
+
+class LeadViewSet(viewsets.ModelViewSet):
+    queryset = Lead.objects.all()
+    permission_classes = [
+        permissions.AllowAny
+    ]
+    serializer_class = LeadSerializer
--- react-django/leadmanager/lead/models.py
+++ react-django/leadmanager/lead/models.py
@@ -5,6 +5,6 @@ from django.db import models
 
 class Lead(models.Model):
     name = models.CharField(max_length=50)
-    email = models.EmailField(max_length=254, unique=True)
+    email = models.EmailField(max_length=254)
     message = models.CharField(max_length=50, blank=True)
     creat_at = models.DateTimeField(auto_now_add=True)
